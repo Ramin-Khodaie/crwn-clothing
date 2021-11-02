bd865f8a5012a978a450ea581bf69b8bbbd4d76e
Ramin <kh.ramin69@gmail.com>
2021-11-02 13:00:28 -0700

    cartitems added to checkout

--- src/components/CollectionItem/CollectionItem.jsx
+++ src/components/CollectionItem/CollectionItem.jsx
@@ -32,9 +32,14 @@ function CollectionItem({ item }) {
         <span className="name">{name}</span>
         <span className="price">${price}</span>
       </div>
-      <CustomButton inverted onClick={() => dispatch(cartItems(item))}>
+      <CustomButton inverted onClick={() => dispatch(addItem(item))}>
         Add to cart
       </CustomButton>
+      
+      {//here using cartSlice items added to state,using addItem2 action
+      /* <CustomButton inverted onClick={() => dispatch(cartItems(item))}>
+        Add to cart
+      </CustomButton> */}
     </div>
   );
 }
--- src/pages/Chechout/Checkoutpage.jsx
+++ src/pages/Chechout/Checkoutpage.jsx
@@ -1,47 +1,58 @@
 import { createStructuredSelector } from "reselect";
-import {
-  selectaCartItem,
-  selectCartItemsCount,
-  cartItemTotal,
-} from "../../redux/cart/cartSelectore";
+// import {
+//   selectaCartItem,
+//   selectCartItemsCount,
+//   cartItemTotal,
+// } from "../../redux/cart/cartSelectore";
 
 import { connect } from "react-redux";
 
 import "./checkout.scss";
 import Chekout from "../../components/Checkout/Checkout";
 import StripButton from "../../components/StripButton/Strip-button";
+import { useSelector } from "react-redux";
 
-const CheckoutPage = ({ cartItems, Total }) => (
-  <div className="checkout-page">
-    <div className="checkout-header">
-      <div className="header-block">
-        <span>Product</span>
-      </div>
-      <div className="header-block">
-        <span>Description</span>
-      </div>
-      <div className="header-block">
-        <span>Quantity</span>
-      </div>
-      <div className="header-block">
-        <span>price</span>
-      </div>
-      <div className="header-block">
-        <span>Remove</span>
-      </div>
-    </div>
-    {cartItems.map((cartitem) => (
-      <Chekout cartItem={cartitem} />
-    ))}
-    <div className="total">
-      <span>Total:{Total}</span>
-    </div>
-    <StripButton price={cartItems.price} />
-  </div>
-);
+const CheckoutPage = () => {
+  const { cartItems } = useSelector((state) => state.cartitem);
 
-const mapStateToProps = createStructuredSelector({
-  cartItems: selectaCartItem,
-  Total: cartItemTotal,
-});
-export default connect(mapStateToProps)(CheckoutPage);
+  const totalCartItems = (items) =>(
+
+    items.reduce((acc,item)=>acc + item.quantity * item.price,0)
+  )
+  
+
+  const total = totalCartItems(cartItems)
+  console.log(98, total);
+  return (
+    <div className="checkout-page">
+      <div className="checkout-header">
+        <div className="header-block">
+          <span>Product</span>
+        </div>
+        <div className="header-block">
+          <span>Description</span>
+        </div>
+        <div className="header-block">
+          <span>Quantity</span>
+        </div>
+        <div className="header-block">
+          <span>price</span>
+        </div>
+        <div className="header-block">
+          <span>Remove</span>
+        </div>
+      </div>
+      {cartItems.map((cartitem) => (
+        <Chekout cartItem={cartitem} />
+      ))}
+      <div className="total">{ <span>Total:{total}</span> }</div>
+      <StripButton price={cartItems.price} />
+    </div>
+  );
+};
+
+// const mapStateToProps = createStructuredSelector({
+//   cartItems: selectaCartItem,
+//   Total: cartItemTotal,
+// });
+export default CheckoutPage;
